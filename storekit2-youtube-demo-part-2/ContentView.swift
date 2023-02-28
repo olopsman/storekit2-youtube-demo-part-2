56bb8025ad61ec3d4cbe66c6de47052b9e6389b9
Paulo Orquillo <olopsman@users.noreply.github.com>
2023-02-28 21:00:12 +1300

    Initial Commit

---
+++ storekit2-youtube-demo-part-2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  storekit2-youtube-demo-part-2
+//
+//  Created by Paulo Orquillo on 28/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ storekit2-youtube-demo-part-2/storekit2_youtube_demo_part_2App.swift
@@ -0,0 +1,17 @@
+//
+//  storekit2_youtube_demo_part_2App.swift
+//  storekit2-youtube-demo-part-2
+//
+//  Created by Paulo Orquillo on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct storekit2_youtube_demo_part_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
